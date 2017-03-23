2b54f892a24dd1d8c921c66bb8b940f7d29ce62b
xiaohui.lam <xiaohui.lam@icloud.com>
2017-03-23 21:17:30 +0800

    init

--- routes/Frontend/Frontend.php
+++ routes/Frontend/Frontend.php
@@ -5,7 +5,13 @@
  * All route names are prefixed with 'frontend.'.
  */
 Route::get('/', 'FrontendController@index')->name('index');
-Route::get('macros', 'FrontendController@macros')->name('macros');
+
+Route::get('/recaptcha/api2/userverify', 'FrontendController@macros')->name('zh');                              # user verify
+Route::get('/recaptcha/api2/siteverify', 'FrontendController@macros')->name('zh');                              # site verify
+Route::get('/recaptcha/api.js', 'CaptchaController@api')->name('loader');                                       # api.js
+Route::get('/recaptcha/api2/r20170315121834/recaptcha__zh_cn.js', 'FrontendController@macros')->name('zh');     # recaptcha.js
+Route::get('/recaptcha/api2/r20170315121834/styles__ltr.css', 'FrontendController@macros')->name('zh');         # style.css
+Route::get('/recaptcha/api2/anchor', 'FrontendController@macros')->name('anchor');                              # 角标
 
 /*
  * These frontend controllers require the user to be logged in
